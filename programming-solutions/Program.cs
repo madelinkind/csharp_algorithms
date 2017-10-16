c304a0cc1a552351ed1ef9404aeb0cafb62b90c3
ryuzakyl <ryuzakyl@gmail.com>
2017-10-16 03:44:15 -0400

    add VS solution

---
+++ programming-solutions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programming_solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    static class ConditionalsExercises
+    {
+        public static void ExampleMethod()
+        { }
+    }
+}
