4cc13d43f0ee49ff4e37f169534d82f358d58a35
madelin.diaz <madelin.diaz@datys.cu>
2017-10-27 13:29:24 -0400

    Implementando ejercicio 1, 2, 3 y 4.

--- programming-solutions/Program.cs
+++ programming-solutions/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace programming_solutions
 {
@@ -10,12 +11,111 @@ namespace programming_solutions
     {
         static void Main(string[] args)
         {
+            //NameAgeOfUser();
+            //DoubleName();
+            //TotalTimeDoubleName();
+            //TotalTimeDoubleName1();
+            shorterTime();
         }
-    }
 
-    static class ConditionalsExercises
-    {
-        public static void ExampleMethod()
-        { }
+
+        static void NameAgeOfUser()
+        {
+
+            Console.WriteLine("Hola, teclee su nombre y edad por favor");
+            Console.WriteLine("Nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Edad:");
+            int edad = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Hola " + nombre + ". Felicidades por tus " + edad + " años de edad.");
+            System.Console.Read();
+        }
+
+        static void DoubleName()
+        {
+            Stopwatch crono = new Stopwatch();
+            Console.WriteLine("Hola, teclee su nombre dos veces por favor");
+            for (int i = 0; i < 2; i++)
+            {
+                crono.Start();
+                Console.WriteLine("Nombre:");
+                string nombre = Console.ReadLine();
+                crono.Stop();
+                Console.WriteLine("Te has demorado " + crono.ElapsedMilliseconds + "ms en teclear tu nombre");
+            }
+
+            System.Console.Read();
+
+        }
+
+        static void TotalTimeDoubleName1()
+        {
+            float totalTime = 0;
+            float averageTime = 0;
+            Stopwatch crono = new Stopwatch();
+            Console.WriteLine("Hola, teclee su nombre dos veces por favor");
+            crono.Start();
+            Console.WriteLine("Nombre:");
+            string nombre = Console.ReadLine();
+            crono.Stop();
+            float time = crono.ElapsedMilliseconds;
+            crono.Start();
+            Console.WriteLine("Nombre:");
+            string nombre1 = Console.ReadLine();
+            crono.Stop();
+            float time1 = crono.ElapsedMilliseconds;
+            totalTime = time + time1;
+            averageTime = totalTime / 2;
+            Console.WriteLine("Te has demorado un total de " + totalTime + " ms en escribir dos veces tu nombre");
+            Console.WriteLine("Te has demorado un promedio de " + averageTime + " ms en escribir dos veces tu nombre");
+            System.Console.Read();
+        }
+        static void TotalTimeDoubleName()
+        {
+            float totalTime = 0;
+            float averageTime = 0;
+            Stopwatch crono = new Stopwatch();
+            Console.WriteLine("Hola, teclee su nombre dos veces por favor");
+
+            for (int i = 0; i < 2; i++)
+            {
+                crono.Start();
+                Console.WriteLine("Nombre:");
+                string nombre = Console.ReadLine();
+                crono.Stop();
+                float time = crono.ElapsedMilliseconds;
+                totalTime += time;
+            }
+            averageTime = totalTime / 2;
+            Console.WriteLine("Te has demorado un total de " + totalTime + " ms en escribir dos veces tu nombre");
+            Console.WriteLine("Te has demorado un promedio de " + averageTime + " ms en escribir dos veces tu nombre");
+            System.Console.Read();
+        }
+
+        static void shorterTime()
+        {
+            Stopwatch crono = new Stopwatch();
+            Console.WriteLine("Hola, teclee su nombre dos veces por favor");
+            crono.Start();
+            Console.WriteLine("Nombre:");
+            string nombre = Console.ReadLine();
+            crono.Stop();
+            float time = crono.ElapsedMilliseconds;
+            crono.Start();
+            Console.WriteLine("Nombre:");
+            string nombre1 = Console.ReadLine();
+            crono.Stop();
+            float time1 = crono.ElapsedMilliseconds;
+            float shorter = Math.Min(time, time1);
+            Console.Clear();
+            Console.WriteLine("El tiempo menor es " + shorter);
+            System.Console.Read();
+
+        }
+
     }
 }
+
+
+
