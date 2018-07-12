e463c65425c4f3062614d7a29f04940aeb99f466
Unknown <madelinkind@gmail.com>
2018-07-11 22:29:47 -0400

    add exercises for week 2

---
+++ programming-solutions/Semana2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace programming_solutions
+{
+    static class Semana2
+    {
+        public static void Suma(int a, int b)
+        {
+            int suma = a + b;
+            Console.WriteLine("{0}", suma);
+        }
+    }
+}
