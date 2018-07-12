06ad702afeda6c5537660b1bce6ba963144fccc4
Unknown <madelinkind@gmail.com>
2018-07-11 22:29:12 -0400

    add exercises for week 1

---
+++ programming-solutions/Semana1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace programming_solutions
+{
+    static class Semana1
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
