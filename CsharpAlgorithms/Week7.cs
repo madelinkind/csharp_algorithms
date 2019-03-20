8c53a2587fc7c576de73e0addd6b9fc31c2c1880
madelinkind <madelinkind@gmail.com>
2019-03-20 14:42:26 -0400

    adding class week7

--- CsharpAlgorithms/Week7.cs
+++ CsharpAlgorithms/Week7.cs
@@ -6,5 +6,12 @@ namespace CsharpAlgorithms
 {
     public static class Week7
     {
+        public static void SearchBinary(double[] listOrder)
+        {
+            for (int i = 0; i < listOrder.Length; i++)
+            {
+               
+            }
+        }
     }
 }
